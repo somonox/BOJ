bd9aa69b836c9f998b6d5e232ec587a1dfbb163d
a6a6(a7a7) <97081428+a7a7-7@users.noreply.github.com>
2025-03-20 18:08:27 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int num;
+
+int main() {
+    cin >> num;
+
+    if (num % 2 == 0)
+        cout << "CY" << endl;
+    else
+        cout << "SK" << endl;
+
+    return 0;
+}
