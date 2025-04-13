0a3e1e8b5233310abedabca74c15efa344e47aea
a6a6(a7a7) <97081428+a7a7-7@users.noreply.github.com>
2025-04-13 22:58:39 +0900

    [Bronze V] Title: Hello World, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.cc
@@ -0,0 +1,8 @@
+#include<iostream>
+#define i using namespace std;
+#define miss int main() {
+#define the cout << "Hello World!" << endl;
+#define old return 0;
+#define kanye }
+
+i miss the old kanye
