f3446f25062c9ac7a6b5cfc636c6c4eea9d4de6b
a6a6(a7a7) <97081428+a7a7-7@users.noreply.github.com>
2025-04-24 17:43:38 +0900

    [Platinum V] Title: LU 분해, Time: 228 ms, Memory: 26596 KB -BaekjoonHub

--- 백준/Platinum/12020. LU 분해/LU 분해.cc
+++ 백준/Platinum/12020. LU 분해/LU 분해.cc
@@ -1,70 +1,52 @@
-#include <iostream>
+#include<iostream>
+
 using namespace std;
 
-int n;
-double upper[1001][1001], lower[1001][1001];
+double A[1024][1024];
+double L[1024][1024];
+double U[1024][1024];
 
-void print_state() {
-    cout << "L = " << endl;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << lower[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << endl;
-
-    cout << "U = " << endl;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << upper[i][j] << " ";
-        }
-        cout << endl;
-    }
-
-    cout << endl;
-}
+int num;
+int i, j;
 
 int main() {
-    ios_base :: sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    cin >> n;
-    for(int i = 0; i < n; i++) {
-        lower[i][i] = 1;
-        for(int j = 0; j < n; j++)
-            cin >> upper[i][j];
+    cin >> num;
+    for (i = 0; i < num; i++) {
+        for (j = 0; j < num; j++)
+            scanf("%lf", &A[i][j]);
+        L[i][i] = 1;
     }
 
-    if(n==1) {
-        cout << -1;
+    if (num == 1) {
+        printf("-1\n");
         return 0;
     }
 
-    for(int i = 0; i < n; i++) {
-        double pivot = upper[i][i];
-        if(pivot == 0) {
-            cout << -1;
+    U[0][0] = A[0][0];
+    U[0][1] = A[0][1];
+
+    for (i = 1; i < num; i++) {
+        if (U[i - 1][i - 1] == 0) {
+            printf("-1\n");
             return 0;
         }
-
-        double tmp = -(upper[i+1][i]/upper[i][i]);
-        for(int j = 0; j < n; j++) {
-            double val = upper[i][j] * tmp;
-            upper[i+1][j] += val;
-        }
-        lower[i+1][i] = -tmp;
-    }
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++)
-            printf("%.3lf ", lower[i][j]);
-        printf("\n");
-    }
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++)
-            printf("%.3lf ", upper[i][j]);
-        printf("\n");
+        L[i][i - 1] = A[i][i - 1] / U[i - 1][i - 1];
+        U[i][i] = A[i][i] - L[i][i - 1] * U[i - 1][i];
+        if (i < num - 1)
+            U[i][i + 1] = A[i][i + 1];
     }
 
+    for (i = 0; i < num; i++) {
+        for (j = 0; j < num; j++)
+            printf("%.3lf ", L[i][j]);
+        cout << '\n';
+    }
+
+    for (i = 0; i < num; i++) {
+        for (j = 0; j < num; j++)
+            printf("%.3lf ", U[i][j]);
+        cout << '\n';
+    }
+
+    return 0;
 }
