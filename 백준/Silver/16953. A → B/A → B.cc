8c9d7d6d566e77aa82498714350c12db3094f2bd
a6a6(a7a7) <97081428+a7a7-7@users.noreply.github.com>
2025-05-14 15:12:50 +0900

    [Silver II] Title: A → B, Time: 20 ms, Memory: 6780 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.cc
+++ 백준/Silver/16953. A → B/A → B.cc
@@ -1,36 +1,38 @@
 #include<iostream>
-#include<vector>
-#include<climits>
+#include<queue>
+#include<map>
 
 using namespace std;
 
-vector<int> dp;
-bool flag = true;
-unsigned long long n, m, c;
-unsigned long long i;
+queue<pair<long long, int>> q; // long long으로 바꾸는 게 안정적
+map<long long, bool> ch;
+long long a, b;
 
 int main() {
-    cin >> n >> m;
-    
+    ios::sync_with_stdio(false);
+    cout.tie(NULL);
+    cin.tie(NULL);
 
+    cin >> a >> b;
+    q.push({a, 1});
+    while (!q.empty()) {
+        long long fr = q.front().first;
+        int cnt = q.front().second;
+        q.pop();
 
-    for (i = m; i >= n; i--) {
-        if (m == n) {
-            flag = false;
-            break;
+        if (fr == b) {
+            cout << cnt << endl;
+            return 0;
         }
 
-        if (m % 10 == 1)
-            m /= 10;
-        else if (m % 2 == 0)
-            m /= 2;
-        
-        c++;
+        if (ch.count(fr) > 0 || fr > b) continue;
+
+        ch[fr] = true;
+        q.push({fr * 2, cnt + 1});
+        q.push({fr * 10 + 1, cnt + 1});
     }
-    if (flag)
-        cout << "-1" << endl;
-    else
-        cout << c + 1 << endl;
+
+    cout << -1 << endl;
 
     return 0;
 }
