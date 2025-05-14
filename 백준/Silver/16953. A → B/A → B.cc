a09dfb632a57852fdc0f959b8055dede1c2243eb
a6a6(a7a7) <97081428+a7a7-7@users.noreply.github.com>
2025-05-14 15:13:34 +0900

    [Silver II] Title: A → B, Time: 0 ms, Memory: 2552 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.cc
+++ 백준/Silver/16953. A → B/A → B.cc
@@ -1,12 +1,49 @@
+// 그리디 해법
+// #include<iostream>
+// #include<vector>
+// #include<climits>
+
+// using namespace std;
+
+// vector<int> dp;
+// bool flag = true;
+// unsigned long long n, m, c;
+// unsigned long long i;
+
+// int main() {
+//     cin >> n >> m;
+
+//     for (i = m; i >= n; i--) {
+//         if (m == n) {
+//             flag = false;
+//             break;
+//         }
+
+//         if (m % 10 == 1)
+//             m /= 10;
+//         else if (m % 2 == 0)
+//             m /= 2;
+        
+//         c++;
+//     }
+//     if (flag)
+//         cout << "-1" << endl;
+//     else
+//         cout << c + 1 << endl;
+
+//     return 0;
+// }
+
+// 그래프
 #include<iostream>
 #include<queue>
 #include<map>
 
 using namespace std;
 
-queue<pair<long long, int>> q; // long long으로 바꾸는 게 안정적
-map<long long, bool> ch;
-long long a, b;
+queue<pair<unsigned long long, unsigned long long>> q;
+map<unsigned long long, bool> ch;
+unsigned long long a, b;
 
 int main() {
     ios::sync_with_stdio(false);
@@ -16,18 +53,17 @@ int main() {
     cin >> a >> b;
     q.push({a, 1});
     while (!q.empty()) {
-        long long fr = q.front().first;
-        int cnt = q.front().second;
+        unsigned long long fr = q.front().first;
+        unsigned long long cnt = q.front().second;
         q.pop();
 
         if (fr == b) {
             cout << cnt << endl;
             return 0;
         }
-
-        if (ch.count(fr) > 0 || fr > b) continue;
-
-        ch[fr] = true;
+        if(fr > b)
+            continue;
+        
         q.push({fr * 2, cnt + 1});
         q.push({fr * 10 + 1, cnt + 1});
     }
