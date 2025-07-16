756bc9e19331e1404d3c9cc7ac181594ede2a6ca
a6a6(a7a7) <97081428+a7a7-7@users.noreply.github.com>
2025-07-16 23:43:41 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2932 ms, Memory: 48884 KB -BaekjoonHub

---
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<set>
+
+using namespace std;
+
+char cmd;
+int t, v, value;
+int i, j;
+
+int main() {
+    cin >> t;
+
+    for (i = 0; i < t; i++) {
+        multiset<int> dpq;
+        cin >> v;
+        for (j = 0; j < v; j++) {
+            cin >> cmd;
+            cin >> value;
+            if (cmd == 'I') {
+                dpq.insert(value);
+            } else {
+                if (dpq.empty())
+                    continue;
+                if (value == -1)
+                    dpq.erase(dpq.begin());
+                else
+                    dpq.erase(prev(dpq.end()));
+            }
+        }
+        if (dpq.empty())
+            cout << "EMPTY" << '\n';
+        else
+            cout << *prev(dpq.end()) << " " << *dpq.begin() << '\n';
+    }
+
+    return 0;
+}
