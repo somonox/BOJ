347cb49a5c7fdffd3eb9e051ab19d343a29c464a
이형민 <97081428+somonox@users.noreply.github.com>
2025-08-21 10:54:04 +0900

    [Gold III] Title: 소수의 배수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/17436. 소수의 배수/소수의 배수.cc
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+using ll = long long;
+
+vector<ll> v;
+vector<ll> unions;
+vector<ll> complement;
+ll num, pc, unionsum, complementsum;
+ll i, j;
+
+int main() {
+    cin >> pc >> num;
+    v.resize(pc);
+    sort(v.begin(), v.end());
+
+    for (auto &i : v) {
+        cin >> i;
+        unions.push_back(num / i);
+    }
+
+    for (ll mask = 1; mask < (1 << pc); ++mask) {
+        if (__builtin_popcount((unsigned)mask) < 2)
+            continue;
+        vector<ll> comb;
+        for (ll i = 0; i < pc; ++i)
+            if (mask >> i & 1)
+                comb.push_back(v[i]);
+        ll sum = 1;
+        for (ll i = 0; i < comb.size(); ++i) {
+            sum *= comb[i];
+        }
+        if (comb.size() % 2 == 0)
+            sum = -sum;
+        complement.push_back(num / sum);
+    }
+
+    for (auto i :unions) {
+        unionsum += i;
+    }
+
+    for (auto j : complement) {
+        complementsum += j;
+    }
+
+    cout << unionsum + complementsum << endl;
+
+
+    return 0;
+}
