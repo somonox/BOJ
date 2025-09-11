9b9ca0466ef686ee268ed2cad0b664d6d85d6688
이형민 <97081428+somonox@users.noreply.github.com>
2025-09-11 19:59:04 +0900

    [Platinum V] Title: Strongly Connected Component, Time: 24 ms, Memory: 3588 KB -BaekjoonHub

---
+++ 백준/Platinum/2150. Strongly Connected Component/Strongly Connected Component.cc
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int num[10101];
+int low[10101];
+bool visited[10101];
+int cnt = 0;
+vector<int> g[10101];
+stack<int> s;
+vector< vector<int> > ans;
+
+void dfs(int now){
+    visited[now] = 1;
+    s.push(now);
+    num[now] = ++cnt;
+    low[now] = cnt;
+
+    for(auto nxt : g[now]){
+        if(!num[nxt]){
+            dfs(nxt);
+            low[now] = min(low[now], low[nxt]);
+        }else if(visited[nxt])
+            low[now] = min(low[now], num[nxt]);
+    }
+
+    if(low[now] == num[now]){
+        vector<int> scc;
+        while(!s.empty()){
+            int poped = s.top(); s.pop();
+            scc.push_back(poped);
+            visited[poped] = 0;
+            if(now == poped) break;
+        }
+        sort(scc.begin(), scc.end());
+        ans.push_back(scc);
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n, m; cin >> n >> m;
+    while(m--){
+        int a, b; cin >> a >> b;
+        g[a].push_back(b);
+    }
+
+    for(int i=1; i<=n; i++)
+        if(!num[i])
+            dfs(i);
+    cout << ans.size() << "\n";
+
+    sort(ans.begin(), ans.end());
+    for(auto v : ans){
+        for(auto i : v)
+            cout << i << " ";
+        cout << "-1\n";
+    }
+}
